ebf84ca782c59ab4163758946c80327a21c3a41c
Aebroyx <sangalabror@gmail.com>
2023-11-25 15:56:20 +0700

    deploy ready v1

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,13 +1,61 @@
 import Link from "next/link"
+import './page.css'
 
 export default function About() {
     return(
         <>
-            <div className='flex h-screen'>
+            {/* Mobile */}
+            <div id='aboutmobile' className="flex h-screen">
+                <div className='flex flex-col justify-center items-center mx-6'>
+                    <section>
+                        <div className="mb-12">
+                            <p className='text-primary'>
+                                Hi, my name is
+                            </p>
+                            <h1 className='text-4xl font-bold text-white pt-4'>
+                                Sangalabror Pujianto
+                            </h1>
+                            <h2 className='text-3xl font-bold text-primary pt-4'>
+                                Fullstack Developer
+                            </h2>
+                            <br/>
+                            <h3 className='text-white'>
+                                A motivated individual who has been aspiring to be a Software Developer since early age. I have now specialized in full stack software
+                                development. I consider myself to be versatile and can adapt and overcome different challenges. Efficiently skilled
+                                in problem solving. Has worked around technologies at an early age which lead to my pursuit of my Computer
+                                Science degree.
+                            </h3>
+                        </div>
+                        <div className="flex flex-col justfiy-center items-center font-bold text-primary text-4xl gap-5 mb-28">
+                            <div>
+                                <Link href='/education' className="group transition duration-300">
+                                    Education
+                                    <span className="block max-w-0 group-hover:max-w-full transition-all duration-500 h-1 bg-primary"></span>
+                                </Link>
+                            </div>
+                            <div>
+                                <Link href='/experience' className="group transition duration-300">
+                                    Experience
+                                    <span className="block max-w-0 group-hover:max-w-[265px] transition-all duration-500 h-1 bg-primary"></span>
+                                </Link>
+                            </div>
+                            <div>
+                                <Link href='/projects' className="group transition duration-300">
+                                    Projects
+                                    <span className="block max-w-0 group-hover:max-w-[200px] transition-all duration-500 h-1 bg-primary"></span>
+                                </Link>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+
+            {/* Desktop */}
+            <div id='aboutdesktop' className='flex h-screen'>
                 <div className='w-1/12'>
                 </div>
                 <div className='flex-1 flex justify-center items-center'>
-                    <section id='about'>
+                    <section>
                         <p className='text-primary'>Hi, my name is</p>
                         <h1 className='text-7xl font-bold text-white pt-4'>
                         Sangalabror Pujianto
@@ -17,14 +65,14 @@ export default function About() {
                         </h2>
                         <br/>
                         <h3 className='text-white'>
-                        A motivated individual who aspires to be a Software Developer that specializes in full stack software
+                        A motivated individual who has been aspiring to be a Software Developer since early age. I have now specialized in full stack software
                         development. I consider myself to be versatile and can adapt and overcome different challenges. Efficiently skilled
                         in problem solving. Has worked around technologies at an early age which lead to my pursuit of my Computer
                         Science degree.
                         </h3>
                     </section>
                 </div>
-                <div className="hidden lg:flex-1 flex justify-center items-center">
+                <div id="selector" className="flex-1 flex justify-center items-center">
                     <div className="flex flex-col font-bold text-primary text-5xl gap-5 ml-32">
                         <div>
                             <Link href='/education' className="group transition duration-300">
--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -6,14 +6,14 @@ export default function Footer() {
     return(
         <>
             <footer className="pb-12 mt-[-120px]">
-                <div className="flex justify-center items-center gap-5">
-                    <a href="https://www.linkedin.com/in/sangalabrorpujianto/" className="text-4xl text-primary">
+                <div className="flex justify-center items-center gap-8">
+                    <a href="https://www.linkedin.com/in/sangalabrorpujianto/" className="text-4xl text-primary hover:text-6xl">
                         <FaLinkedin />
                     </a>
-                    <a href="https://github.com/Aebroyx" className="text-4xl text-primary">
+                    <a href="https://github.com/Aebroyx" className="text-4xl text-primary hover:text-6xl">
                         <FaGithub />
                     </a>
-                    <a href="https://www.instagram.com/abrorpujianto/" className="text-4xl text-primary">
+                    <a href="https://www.instagram.com/abrorpujianto/" className="text-4xl text-primary hover:text-6xl">
                         <FaInstagram />
                     </a>
                 </div>
