ed76a25e64df46d7c2b4348a227c6f2c48d7d158
Aebroyx <sangalabror@gmail.com>
2026-02-03 10:08:00 +0700

    feat: enforce static rendering for StudioPage to prevent cold start timeouts

--- src/app/(studio)/studio/[[...tool]]/page.tsx
+++ src/app/(studio)/studio/[[...tool]]/page.tsx
@@ -3,6 +3,9 @@
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../../../../sanity.config'
 
+// Force this route to be static to avoid cold start timeouts
+export const dynamic = 'force-static'
+
 export default function StudioPage() {
   return <NextStudio config={config} />
 }
